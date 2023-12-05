5f47a760cf34272ecf847f545c785d030cbca003
Your Name <you@example.com>
2023-12-05 16:05:21 +1100

    menu

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './Navbar.module.scss';
-import Logo from './Logo';
+import Logo from '../../img/logo.svg';
 import Menu from './Menu';
 
 const Navbar = () => {
@@ -8,7 +8,11 @@ const Navbar = () => {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <Logo />
+        <div className={styles.imgContainer}>
+          <img src={Logo} alt='logo'/>
+        </div>
+        
+        
         <Menu />
         <a className={styles.author} href='https://www.linkedin.com/in/kevin-chong-ng-49478526a/' target='_blank'>
           
